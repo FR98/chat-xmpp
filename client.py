0c0b6ee3e23ad70cc737bd151b37c86057653166
FranciscoRosal <ros18676@uvg.edu.gt>
2021-08-06 18:57:46 -0600

    feat: client

---
+++ client.py
@@ -0,0 +1,71 @@
+"""
+    Author: Francisco Rosal
+"""
+
+# alumchat.xyz
+
+class Client(object):
+    
+    def __init__(self):
+        pass
+
+    def register(self):
+        # Registrar una nueva cuenta en el servidor
+        pass
+
+    def login(self):
+        # Iniciar sesión con una cuenta
+        pass
+
+    def logout(self):
+        # Cerrar sesión con una cuenta
+        pass
+
+    def destroy_account(self):
+        # Eliminar la cuenta del servidor
+        pass
+
+    def list_users(self):
+        # Mostrar todos los usuarios y su estado
+        pass
+
+    def list_contacts(self):
+        # Mostrar todos los contactos y su estado
+        pass
+
+    def add_user_to_contacts(self):
+        # Agregar un usuario a los contactos
+        pass
+
+    def show_user_detail(self):
+        # Mostrar detalles de contacto de un usuario
+        pass
+
+    def private_chat(self):
+        # Comunicación 1 a 1 con cualquier usuario/contacto
+        pass
+
+    def group_chat(self):
+        # Participar en conversaciones grupales
+        pass
+
+    def set_presence_message(self):
+        # Definir mensaje de presencia
+        pass
+
+    def send_notifications(self):
+        # Enviar/recibir notificaciones
+        pass
+
+    def receive_notifications(self):
+        # Recibir notificaciones
+        pass
+
+    def send_files(self):
+        # Enviar/recibir archivos
+        pass
+
+    def receive_files(self):
+        # Recibir archivos
+        pass
+
