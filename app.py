f4d160c0cb313a0c0749ba7c7a46c5136728aab5
FranciscoRosal <ros18676@uvg.edu.gt>
2021-08-08 00:28:14 -0600

    feat: improvements

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 import logging
 from argparse import ArgumentParser
 
-from slixmpp import ClientXMPP
+from client import Client
 
 # Setup the command line arguments and logging.
 parser = ArgumentParser()
@@ -13,124 +13,169 @@ parser.add_argument("-q", "--quiet", help="set logging to ERROR", action="store_
 parser.add_argument("-d", "--debug", help="set logging to DEBUG", action="store_const", dest="loglevel", const=logging.DEBUG, default=logging.INFO)
 args = parser.parse_args()
 
-# logging.basicConfig(level=logging.DEBUG, format='%(levelname)-8s %(message)s')
-logging.basicConfig(level=args.loglevel, format='%(levelname)-8s %(message)s')
-# log = logging.getLogger(__name__)
-# log.warning('You should catch IqTimeout exceptions')
+# logging.basicConfig(level=logging.DEBUG, format="%(levelname)-8s %(message)s")
+logging.basicConfig(level=args.loglevel, format="%(levelname)-8s %(message)s")
+log = logging.getLogger(__name__)
+# log.error("Command not found 1")
+# log.warning("Command not found 2")
+# log.critical("Command not found 3")
+# log.debug("Command not found 4")
+# log.exception("Command not found 5")
+# log.info("Command not found 6")
 
 
 
 
 
-class Client(ClientXMPP):
+class App(object):
 
     def __init__(self):
-        self.jid = None
+        self.client = None
+        self.current_user_jid = None
         self.password = None
         self.running = True
         self.authenticated = False
+        self.app()
 
-        client.register_plugin('xep_0030') # Service Discovery
-        client.register_plugin('xep_0199') # XMPP Ping
 
     def app(self):
         while self.running:
             if self.authenticated:
-                self.unauthenticated_menu()
-            else:
                 self.authenticated_menu()
+            else:
+                self.unauthenticated_menu()
 
-            input("NEXT")
+            try:
+                option = input("> ")
+                exec("self.{}()".format(option.lower()))
+            except:
+                log.error("Command not found 1")
+
 
     def unauthenticated_menu(self):
         print("""
------------------
+    =============
         Menu:
-    1. Register
-    2. Login
------------------
+    -------------
+    · Close
+    · Register
+    · Login
+    =============
         """)
+
 
     def authenticated_menu(self):
         print("""
------------------
+    =============
         Menu:
-    1. Logout
------------------
+    -------------
+    · Close
+    · Logout
+    =============
         """)
 
+
     async def start(self, event):
-        await self.get_roster()
-        self.private_chat()
+        # await self.get_roster()
+        # self.private_chat()
+        pass
+
 
     def register(self):
         # Registrar una nueva cuenta en el servidor
         pass
 
+
     def login(self):
         # Iniciar sesión con una cuenta
-        self.jid = 'test@alumchat.xyz'
-        self.password = '12345'
+        self.current_user_jid = "test@alumchat.xyz"
+        self.password = "12345"
 
-        super().__init__(self.jid, self.password)
-        client.connect()
-        client.process(forever=False)
+        self.client = Client(self.current_user_jid, self.password, "echobot@alumchat.xyz", "Test message 2")
+        self.client.register_plugin("xep_0030") # Service Discovery
+        self.client.register_plugin("xep_0199") # XMPP Ping
 
-        self.add_event_handler('session_start', self.start)
+        # Connect to the XMPP server and start processing XMPP stanzas.
+        self.client.connect()
+        self.client.process(forever=False)
+
+
+
+        # super().__init__(self.current_user_jid, self.password)
+        # client.connect()
+        # client.process(forever=False)
+
+        # self.add_event_handler("session_start", self.start)
+
 
     def logout(self):
         # Cerrar sesión con una cuenta
-        self.disconnect()
+        # self.disconnect()
+        pass
+
 
     def close(self):
         self.running = False
+
 
     def destroy_account(self):
         # Eliminar la cuenta del servidor
         pass
 
+
     def list_users(self):
         # Mostrar todos los usuarios y su estado
         pass
+
 
     def list_contacts(self):
         # Mostrar todos los contactos y su estado
         pass
 
+
     def add_user_to_contacts(self):
         # Agregar un usuario a los contactos
         pass
+
 
     def show_user_detail(self):
         # Mostrar detalles de contacto de un usuario
         pass
 
+
     def private_chat(self):
         # Comunicación 1 a 1 con cualquier usuario/contacto
-        receiver_jid = 'echobot@alumchat.xyz'
+        receiver_jid = "echobot@alumchat.xyz"
         message = input("Message")
-        self.send_message(mto=receiver_jid, mbody=message, mtype='chat')
+        # self.send_message(mto=receiver_jid, mbody=message, mtype="chat")
         pass
+
 
     def group_chat(self):
         # Participar en conversaciones grupales
         pass
 
+
     def set_presence_message(self):
         # Definir mensaje de presencia
-        self.send_presence()
+        # self.send_presence()
+        pass
+
 
     def send_notifications(self):
         # Enviar/recibir notificaciones
         pass
 
+
     def receive_notifications(self):
         # Recibir notificaciones
         pass
 
+
     def send_files(self):
         # Enviar/recibir archivos
         pass
+
 
     def receive_files(self):
         # Recibir archivos
@@ -138,5 +183,5 @@ class Client(ClientXMPP):
 
 
 
-if __name__ == '__main__':
-    client = Client()
+if __name__ == "__main__":
+    App()
--- client.py
+++ client.py
@@ -2,25 +2,7 @@
     Author: Francisco Rosal
 """
 
-import logging
-from argparse import ArgumentParser
-
 from slixmpp import ClientXMPP
-
-# Setup the command line arguments and logging.
-parser = ArgumentParser()
-parser.add_argument("-q", "--quiet", help="set logging to ERROR", action="store_const", dest="loglevel", const=logging.ERROR, default=logging.INFO)
-parser.add_argument("-d", "--debug", help="set logging to DEBUG", action="store_const", dest="loglevel", const=logging.DEBUG, default=logging.INFO)
-args = parser.parse_args()
-
-# logging.basicConfig(level=logging.DEBUG, format='%(levelname)-8s %(message)s')
-logging.basicConfig(level=args.loglevel, format='%(levelname)-8s %(message)s')
-# log = logging.getLogger(__name__)
-# log.warning('You should catch IqTimeout exceptions')
-
-
-
-
 
 class Client(ClientXMPP):
 
@@ -34,6 +16,7 @@ class Client(ClientXMPP):
 
         self.add_event_handler('session_start', self.start)
 
+
     async def start(self, event):
         self.send_presence()
         await self.get_roster()
@@ -41,73 +24,77 @@ class Client(ClientXMPP):
         self.send_message(mto=self.jid_receiver, mbody=self.message, mtype='chat')
         self.disconnect()
 
+
     def register(self):
         # Registrar una nueva cuenta en el servidor
         pass
+
 
     def login(self):
         # Iniciar sesión con una cuenta
         pass
 
+
     def logout(self):
         # Cerrar sesión con una cuenta
         pass
+
 
     def destroy_account(self):
         # Eliminar la cuenta del servidor
         pass
 
+
     def list_users(self):
         # Mostrar todos los usuarios y su estado
         pass
+
 
     def list_contacts(self):
         # Mostrar todos los contactos y su estado
         pass
 
+
     def add_user_to_contacts(self):
         # Agregar un usuario a los contactos
         pass
+
 
     def show_user_detail(self):
         # Mostrar detalles de contacto de un usuario
         pass
 
+
     def private_chat(self):
         # Comunicación 1 a 1 con cualquier usuario/contacto
         pass
+
 
     def group_chat(self):
         # Participar en conversaciones grupales
         pass
 
+
     def set_presence_message(self):
         # Definir mensaje de presencia
         pass
+
 
     def send_notifications(self):
         # Enviar/recibir notificaciones
         pass
 
+
     def receive_notifications(self):
         # Recibir notificaciones
         pass
+
 
     def send_files(self):
         # Enviar/recibir archivos
         pass
 
+
     def receive_files(self):
         # Recibir archivos
         pass
-
-
-
-if __name__ == '__main__':
-    client = Client('test@alumchat.xyz', '12345', 'echobot@alumchat.xyz', 'Test message')
-    client.register_plugin('xep_0030') # Service Discovery
-    client.register_plugin('xep_0199') # XMPP Ping
-
-    # Connect to the XMPP server and start processing XMPP stanzas.
-    client.connect()
-    client.process(forever=False)
